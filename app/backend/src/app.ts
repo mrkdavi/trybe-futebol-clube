29bcc84be949d223ff42cb56af572f11d5d042ea
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-11-25 12:25:41 -0300

    Fixing dockerfile

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import createRoutes from './routes';
 
 class App {
   public app: express.Express;
@@ -22,6 +23,7 @@ class App {
 
     this.app.use(express.json());
     this.app.use(accessControl);
+    createRoutes(this.app);
   }
 
   public start(PORT: string | number):void {
