a3740007bccdc44fd9351bbd559a3efbe00d9a44
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-12-01 11:32:30 -0300

    mocking loginModel at 01_session.test.ts

---
+++ app/backend/src/tests/mocks/login.ts
@@ -0,0 +1,7 @@
+export const loginReturnMock = {
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjY5NjUyNzg4fQ.ymgFc4IodANpa8yeumJ6cNggVSRSj_HhP2HvkySDVyk',
+}
+
+export const validateLoginReturnMock = {
+  role: 'admin'
+}
